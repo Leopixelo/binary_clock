39a558590efc4936d248ab7cb34be0acb8a988b3
Leopixelo <leonardgies01@gmail.com>
2024-07-01 22:51:34 +0200

    tested led strip

--- binary_clock/binary_clock.ino
+++ binary_clock/binary_clock.ino
@@ -3,24 +3,35 @@
 
 #define LED_BUILTIN 15
 
-const int NUM_PIXELS = 10;
+// const int NUM_PIXELS = 104;
+const int NUM_PIXELS = 77;
 const int LED_PIN = 16;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {  //
-    // pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
     pixels.begin();
+
+    Serial.begin(115200);
 }
 
 void loop() {
-    // digitalWrite(LED_BUILTIN, HIGH);
-    // delay(1000);
-    // digitalWrite(LED_BUILTIN, LOW);
-    // delay(1000);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+
+    Serial.write("testing");
 
     for (int i = 0; i < NUM_PIXELS; i++) {
-        pixels.setPixelColor(i, pixels.Color(64, 16, 32));
+        for (int j = 0; j < NUM_PIXELS; j++) {
+            if (j == i) {
+                pixels.setPixelColor(j, pixels.Color(16, 16, 16));
+            } else {
+                pixels.setPixelColor(j, pixels.Color(0, 0, 0));
+            }
+        }
         pixels.show();
         delay(100);
     }
